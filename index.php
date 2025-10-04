5eef85ee698210b1ebb87feea393e3d527e5d810
Kenneth C Ashwood jr <ashwood.kenneth@gmail.com>
2025-10-04 18:51:26 -0400

    firstcommit

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/css/home.css">
+    <title>Home</title>
+</head>
+<body>
+  <div id=logoBox>
+    <a href="">
+        <img id=logo src="content/Logo.png" alt="">
+    </a>
+</div> 
+<nav id=navigation >
+    <div id=links>
+        <ul>
+            <a href="">About</a>
+            <a href="">Get A Quote</a>
+            <a href="">Contact Us</a>
+        </ul>
+    </div>
+    <div></div>
+
+</nav>
+</body>
+</html>
