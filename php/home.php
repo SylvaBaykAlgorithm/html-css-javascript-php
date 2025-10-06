0b08214ccbe084daeaf04e6af28aed681a8bb368
Kenneth C Ashwood jr <ashwood.kenneth@gmail.com>
2025-10-05 23:43:36 -0400

    Added stying and content for home page

--- php/home.php
+++ php/home.php
@@ -1,1 +1,14 @@
-Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam quod itaque blanditiis recusandae dolor, iste sit a, pariatur dolorem fugiat quia vel quo. Odit maiores explicabo suscipit illo, dolores placeat.
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="/css/home.css">
+    <title>Home</title>
+</head>
+<body>
+    <h1>Home</h1>
+    <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum molestias quidem quas dolorem sit impedit sed consequuntur doloribus? Deserunt, sit autem magni possimus omnis corrupti facere reprehenderit nemo cupiditate architecto?</h1>
+
+</body>
+</html>
